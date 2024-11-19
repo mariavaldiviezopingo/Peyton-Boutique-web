74325f405f4b13f3b69759a5070c6525aa336d22
Arlie Rivas <arliedamir@gmail.com>
2024-11-19 14:53:01 -0500

    Añadir tipo de fuente de la aplicación  y colores en la configuración de Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#00A48A",
+        secondary: "#003C4B",
+      },
+    },
   },
   plugins: [],
 };
