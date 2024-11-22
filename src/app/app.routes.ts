612567172dcdc064dc6f85f93e127b0eae652dc1
Alvaro Saca Caro <alvaro4356@gmail.com>
2024-11-22 13:33:30 -0500

    Componentes: Footer y Register - Alvaro Saca - 22/11/2024 13:27

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { FooterComponent } from './public/footer/footer.component';
+import { RegisterComponent } from './public/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'public/footer', component: FooterComponent },
+    { path: 'public/register', component: RegisterComponent }
+];
