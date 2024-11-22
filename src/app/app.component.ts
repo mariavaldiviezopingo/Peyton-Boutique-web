5038c8121728385389afd6ac74abfef19952eac7
mariavaldiviezopingo <luisavaldiviezo24@gmail.com>
2024-11-21 23:21:16 -0500

    nuevos cambios de merge nuevos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+<<<<<<< HEAD
 import { HeaderComponent } from './components';
 import { SignupComponent } from './public/signup/signup.component';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, SignupComponent],
+=======
+import { LandingComponent } from './public/landing/landing.component'; // Importa el componente Landing
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, LandingComponent],
+>>>>>>> 9bc3cd2 (nuevos cambios de luisa)
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
