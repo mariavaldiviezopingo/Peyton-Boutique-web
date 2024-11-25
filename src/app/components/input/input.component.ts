1a0c55326684fc393aeec3de060be7297e8ec977
Arlie Rivas <arliedamir@gmail.com>
2024-11-25 12:08:09 -0500

    Añadir componente de input con estilos definidos

---
+++ src/app/components/input/input.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-input',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class InputComponent {
+  control = input.required<FormControl>();
+  label = input.required<string>();
+  type = input.required<string>();
+  placeholder = input.required<string>();
+  errorMessage = input.required<string>();
+}
