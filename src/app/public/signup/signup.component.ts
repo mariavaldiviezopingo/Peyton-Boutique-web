99a40d57ee15117e1270c47b335434dd9be480ee
Arlie Rivas <arliedamir@gmail.com>
2024-11-25 12:20:17 -0500

    Añadir componente de signup en formato web

---
+++ src/app/public/signup/signup.component.ts
@@ -0,0 +1,43 @@
+import { NgOptimizedImage } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  Signal,
+} from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { InputComponent } from '@app/components';
+
+interface SignupForm {
+  name: FormControl<string>;
+  email: FormControl<string>;
+  password: FormControl<string>;
+}
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [NgOptimizedImage, InputComponent],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SignupComponent {
+  signupForm: Signal<FormGroup> = computed(
+    () =>
+      new FormGroup<SignupForm>({
+        name: new FormControl('', {
+          nonNullable: true,
+          validators: [Validators.required],
+        }),
+        email: new FormControl('', {
+          nonNullable: true,
+          validators: [Validators.required, Validators.email],
+        }),
+        password: new FormControl('', {
+          nonNullable: true,
+          validators: [Validators.required],
+        }),
+      })
+  );
+}
