4301c83dff0399e10bf2a11380b1ae60dba17302
Arlie Rivas <arliedamir@gmail.com>
2024-11-25 12:07:10 -0500

    Añadir colores y estilos para botones en Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     extend: {
       colors: {
         primary: "#00A48A",
+        "primary-dark": "#005042",
         secondary: "#003C4B",
+        "secondary-dark": "#008BAA",
       },
     },
   },
