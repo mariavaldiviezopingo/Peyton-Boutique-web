1a9779811822d498916f109f96f817db2bae8954
mariavaldiviezopingo <luisavaldiviezo24@gmail.com>
2024-11-27 21:13:24 -0500

    cambios actualziados con la rama de Alvaro

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router'; // Importa RouterModule
+
 
 @Component({
   selector: 'app-footer',
+  standalone: true,
+  imports: [NgOptimizedImage, RouterModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FooterComponent {
 
