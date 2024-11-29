f3aa4376721296cb114f2f85b3f284fb87ed5c40
mariavaldiviezopingo <luisavaldiviezo24@gmail.com>
2024-11-29 01:58:13 -0500

    cambios mejorados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './public/landing/landing.component';
 import {SignupComponent} from './public/signup/signup.component'
-import { RegisterComponent } from './public/register/register.component';
+import { LoginComponent } from './public';
 
 export const routes: Routes = [
-    { path: 'login', component: SignupComponent }, // Ruta login
-    { path: 'register', component: RegisterComponent }, // Ruta register
+    { path: 'login', component: LoginComponent }, // Ruta login
+    { path: 'register', component: SignupComponent  }, // Ruta register
     { path: '', component: LandingComponent }, // Ruta raíz
 
 ];
