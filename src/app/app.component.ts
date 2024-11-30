ce93f11e8dc84ea7701f9a4253adef036950d54e
Alvaro Saca Caro <alvaro4356@gmail.com>
2024-11-30 02:02:57 -0500

    commit de master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components';
 import { SignupComponent } from './public/signup/signup.component';
+import { FooterComponent } from './public/footer/footer.component'; 
+import { LoginComponent } from './public/login/login.component'; 
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SignupComponent],
+  imports: [RouterOutlet, HeaderComponent, SignupComponent,FooterComponent,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
