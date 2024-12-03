0af6ed598ba55bd94f88bacab8058171d7b841df
Arlie Rivas <arliedamir@gmail.com>
2024-12-03 13:32:23 -0500

    Corregir iconos del footer y añadir rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,10 @@
 import { Routes } from '@angular/router';
-import { LandingComponent } from './public/landing/landing.component';
-import {SignupComponent} from './public/signup/signup.component'
 import { LoginComponent } from './public';
+import { LandingComponent } from './public/landing/landing.component';
+import { SignupComponent } from './public/signup/signup.component';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent }, // Ruta login
-    { path: 'register', component: SignupComponent  }, // Ruta register
-    { path: '', component: LandingComponent }, // Ruta raíz
-
+  { path: 'login', component: LoginComponent }, // Ruta login
+  { path: 'signup', component: SignupComponent }, // Ruta register
+  { path: '', component: LandingComponent }, // Ruta raíz
 ];
-
-
