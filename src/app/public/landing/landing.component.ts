b4a1ded7741e97e35f4fe359756681fd414ca5ed
Arlie Rivas <arliedamir@gmail.com>
2024-12-04 10:39:09 -0500

    Optimizar imports en componente landing

--- src/app/public/landing/landing.component.ts
+++ src/app/public/landing/landing.component.ts
@@ -1,23 +1,33 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import {BannerComponent} from '../components/banner/banner.component';
-import {CategoriasComponent} from '../components/categorias/categorias.component';
-import { CampaniaComponent } from '../components/campania/campania.component';
-import { DestacadosComponent } from '../components/destacados/destacados.component';
-import { SuscripcionComponent } from '../components/suscripcion/suscripcion.component';
-import { TestimoniosComponent } from '../components/testimonios/testimonios.component';
-import { ProductosComponent } from '../components/productos/productos.component';
-import { OfertasComponent } from '../components/ofertas/ofertas.component';
-import { InfoComponent } from '../components/info/info.component';
-import { NuevosComponent } from '../components/nuevos/nuevos.component';
+import {
+  BannerComponent,
+  CampaniaComponent,
+  CategoriasComponent,
+  DestacadosComponent,
+  InfoComponent,
+  NuevosComponent,
+  OfertasComponent,
+  ProductosComponent,
+  SuscripcionComponent,
+  TestimoniosComponent,
+} from '../components';
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [BannerComponent, CategoriasComponent, CampaniaComponent, DestacadosComponent, SuscripcionComponent, TestimoniosComponent, ProductosComponent, OfertasComponent,  InfoComponent, NuevosComponent],
+  imports: [
+    BannerComponent,
+    CategoriasComponent,
+    CampaniaComponent,
+    DestacadosComponent,
+    SuscripcionComponent,
+    TestimoniosComponent,
+    ProductosComponent,
+    OfertasComponent,
+    InfoComponent,
+    NuevosComponent,
+  ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-
-export class LandingComponent {
-
-}
+export class LandingComponent {}
