705c2eb8bcc2656d65f9cf0aa3976f04fcd2a331
mariavaldiviezopingo <luisavaldiviezo24@gmail.com>
2024-12-13 17:17:27 -0500

    actualizacion de la rama master

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './public';
 import { LandingComponent } from './public/landing/landing.component';
 import { SignupComponent } from './public/signup/signup.component';
+import { ProductDetailComponent } from './public';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent }, // Ruta login
   { path: 'signup', component: SignupComponent }, // Ruta register
   { path: '', component: LandingComponent }, // Ruta raíz
+  { path: 'detalle', component: ProductDetailComponent }, // Ruta raíz
+
 ];
