81a76e6159272e43074a35cac4ae1bf0f876546e
Arlie Rivas <arliedamir@gmail.com>
2024-12-16 21:48:54 -0500

    Corregir Detection Strategy en banner y productos

--- src/app/public/components/banner/banner.component.ts
+++ src/app/public/components/banner/banner.component.ts
@@ -1,21 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common'; // Importación necesaria para ngFor y ngIf
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   standalone: true,
   imports: [CommonModule], // Importación de CommonModule
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css',], // Corrección en styleUrls (plural)
-  changeDetection: ChangeDetectionStrategy.Default
+  styleUrls: ['./banner.component.css'], // Corrección en styleUrls (plural)
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BannerComponent {
   slides = [
-    { image: 'https://images.unsplash.com/photo-1663970206579-c157cba7edda?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9uZG8lMjBkZSUyMHBhbnRhbGxhJTIwcGFyYSUyMHBjfGVufDB8fDB8fHww', alt: 'Slide 1' },
-    { image: 'https://fotografias.lasexta.com/clipping/cmsimages01/2022/10/06/89BA66AD-53C3-4AFF-8CF7-37175A651B59/fondos-pantalla_98.jpg?crop=1300,731,x0,y19&width=1900&height=1069&optimize=high&format=webply', alt: 'Slide 2' },
-    { image: 'https://marketplace.canva.com/EAFoe6hiEk4/1/0/1600w/canva-fondo-de-pantalla-floral-org%C3%A1nico-rosa-GJjE-IgT0Pg.jpg', alt: 'Slide 3' },
+    {
+      image:
+        'https://images.unsplash.com/photo-1663970206579-c157cba7edda?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9uZG8lMjBkZSUyMHBhbnRhbGxhJTIwcGFyYSUyMHBjfGVufDB8fDB8fHww',
+      alt: 'Slide 1',
+    },
+    {
+      image:
+        'https://fotografias.lasexta.com/clipping/cmsimages01/2022/10/06/89BA66AD-53C3-4AFF-8CF7-37175A651B59/fondos-pantalla_98.jpg?crop=1300,731,x0,y19&width=1900&height=1069&optimize=high&format=webply',
+      alt: 'Slide 2',
+    },
+    {
+      image:
+        'https://marketplace.canva.com/EAFoe6hiEk4/1/0/1600w/canva-fondo-de-pantalla-floral-org%C3%A1nico-rosa-GJjE-IgT0Pg.jpg',
+      alt: 'Slide 3',
+    },
   ];
-  
+
   currentSlide = 0;
 
   selectSlide(index: number): void {
@@ -23,11 +35,11 @@ export class BannerComponent {
   }
 
   prevSlide(): void {
-    this.currentSlide = (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+    this.currentSlide =
+      (this.currentSlide - 1 + this.slides.length) % this.slides.length;
   }
 
   nextSlide(): void {
     this.currentSlide = (this.currentSlide + 1) % this.slides.length;
   }
-
 }
--- src/app/public/components/productos/productos.component.ts
+++ src/app/public/components/productos/productos.component.ts
@@ -1,33 +1,34 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common'; // Importación necesaria para ngFor y ngIf
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { ProductDetailComponent } from '@app/public/product-detail/product-detail.component';
 
 @Component({
   selector: 'app-productos',
   standalone: true,
-  imports: [CommonModule, RouterLink, ProductDetailComponent], // Importación de CommonModule
+  imports: [CommonModule, RouterLink], // Importación de CommonModule
   templateUrl: './productos.component.html',
-  styleUrls: ['./productos.component.css',], // Corrección en styleUrls (plural)
-  changeDetection: ChangeDetectionStrategy.Default
+  styleUrls: ['./productos.component.css'], // Corrección en styleUrls (plural)
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-
 export class ProductosComponent {
   productos = [
     {
       nombre: 'Set de Mochilas con 5 piezas',
       precio: 'S/ 59.90',
-      imagen: 'https://ae01.alicdn.com/kf/S7e1b48f22a0341ba92bdb0908a12255bf.jpg_960x960.jpg',
+      imagen:
+        'https://ae01.alicdn.com/kf/S7e1b48f22a0341ba92bdb0908a12255bf.jpg_960x960.jpg',
     },
     {
       nombre: 'Set de Brochas',
       precio: 'S/ 89.90',
-      imagen: 'https://ae01.alicdn.com/kf/H822a4c1491e440e1b6834aec03d7272eu/Jessup-Juego-de-brochas-de-maquillaje-25-uds-brocha-de-maquillaje-profesional-base-en-polvo-sint.jpg',
+      imagen:
+        'https://ae01.alicdn.com/kf/H822a4c1491e440e1b6834aec03d7272eu/Jessup-Juego-de-brochas-de-maquillaje-25-uds-brocha-de-maquillaje-profesional-base-en-polvo-sint.jpg',
     },
     {
       nombre: 'Juego de Ollas',
       precio: 'S/ 49.90',
-      imagen: 'https://oechsle.vteximg.com.br/arquivos/ids/3870419-800-800/image-641a52c43aa24aa3bcdd63ba74e65a74.jpg?v=637587147934830000',
+      imagen:
+        'https://oechsle.vteximg.com.br/arquivos/ids/3870419-800-800/image-641a52c43aa24aa3bcdd63ba74e65a74.jpg?v=637587147934830000',
     },
   ];
 }
