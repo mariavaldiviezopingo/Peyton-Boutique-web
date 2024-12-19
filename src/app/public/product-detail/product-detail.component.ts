21fa5a02b15712e342eb8f970ce45c67c1f60771
Arlie Rivas <arliedamir@gmail.com>
2024-12-19 11:58:20 -0500

    Mejorar diseño y responsividad de detalle de producto

--- src/app/public/product-detail/product-detail.component.ts
+++ src/app/public/product-detail/product-detail.component.ts
@@ -1,10 +1,11 @@
 import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ProductCardComponent } from '../components';
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [NgOptimizedImage, ProductCardComponent],
+  imports: [NgOptimizedImage, ProductCardComponent, FormsModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -14,5 +15,17 @@ export class ProductDetailComponent {
 
   changeMainImage(newSrc: string): void {
     this.mainImageSrc = newSrc;
+  }
+
+  quantity: number = 1;
+
+  increment() {
+    this.quantity++;
+  }
+
+  decrement() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
   }
 }
