7041c595fe350e91eb3ee4bc0927d96ee389ef2f
sandovaldavid <sandovaldavid2201@gmail.com>
2025-06-09 12:03:31 -0500

    formatear código en el componente de contacto

--- src/app/public/contacto/contacto.component.ts
+++ src/app/public/contacto/contacto.component.ts
@@ -1,14 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router'; 
+import { RouterModule } from '@angular/router';
 
 @Component({
-    selector: 'app-contacto',
-    imports: [RouterModule, CommonModule],
-    templateUrl: './contacto.component.html',
-    styleUrl: './contacto.component.css',
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-contacto',
+  imports: [RouterModule, CommonModule],
+  templateUrl: './contacto.component.html',
+  styleUrl: './contacto.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ContactoComponent {
-
-}
+export class ContactoComponent {}
