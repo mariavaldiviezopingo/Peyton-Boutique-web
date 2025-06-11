77e177b2d072e76bc8154f1c662e76d7fa8cf273
sandovaldavid <sandovaldavid2201@gmail.com>
2025-06-11 18:59:49 -0500

    agregar clave de sitio reCAPTCHA en la configuración del entorno

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
     production: false,
     apiUrl: 'http://localhost:8080/api/',
     authUrl: 'http://localhost:8080',
+    recaptchaSiteKey: '6LeWKF0rAAAAAHEnccFnDnOF4dki8oq-8Cel2NH8',
   };
