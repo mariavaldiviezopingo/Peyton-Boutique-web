80d6e94b7bfe860d943b3171c8c4075c30b2e31d
sandovaldavid <sandovaldavid2201@gmail.com>
2025-06-12 14:54:41 -0500

    formatear componente categoria

--- src/app/public/components/categorias/categorias.component.ts
+++ src/app/public/components/categorias/categorias.component.ts
@@ -3,14 +3,13 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router'; // Importa Router
 
 @Component({
-    selector: 'app-categorias',
-    imports: [CommonModule],
-    templateUrl: './categorias.component.html',
-    styleUrls: ['./categorias.component.css',]
+  selector: 'app-categorias',
+  imports: [CommonModule],
+  templateUrl: './categorias.component.html',
+  styleUrls: ['./categorias.component.css'],
 })
 export class CategoriasComponent implements AfterViewInit {
-
-  constructor(private router: Router) {} 
+  constructor(private router: Router) {}
 
   @ViewChild('carousel') carousel!: ElementRef<HTMLDivElement>;
   isScrollLeftDisabled = true;
@@ -18,15 +17,33 @@ export class CategoriasComponent implements AfterViewInit {
 
   categorias = [
     { nombre: 'Ropa', icon: 'assets/icons/ropa.svg', color: 'bg-blue-100' },
-    { nombre: 'Zapatos', icon: 'assets/icons/zapato.svg', color: 'bg-green-100' },
-    { nombre: 'Accesorios', icon: 'assets/icons/accesorios.svg', color: 'bg-red-100' },
-    { nombre: 'Juguetes', icon: 'assets/icons/juguetes.svg', color: 'bg-orange-100' },
+    {
+      nombre: 'Zapatos',
+      icon: 'assets/icons/zapato.svg',
+      color: 'bg-green-100',
+    },
+    {
+      nombre: 'Accesorios',
+      icon: 'assets/icons/accesorios.svg',
+      color: 'bg-red-100',
+    },
+    {
+      nombre: 'Juguetes',
+      icon: 'assets/icons/juguetes.svg',
+      color: 'bg-orange-100',
+    },
     { nombre: 'Hogar', icon: 'assets/icons/hogar.svg', color: 'bg-purple-100' },
-    { nombre: 'Escuela', icon: 'assets/icons/escuela.svg', color: 'bg-blue-100' },
-    { nombre: 'Belleza', icon: 'assets/icons/Belleza.svg', color: 'bg-green-100' },
-
+    {
+      nombre: 'Escuela',
+      icon: 'assets/icons/escuela.svg',
+      color: 'bg-blue-100',
+    },
+    {
+      nombre: 'Belleza',
+      icon: 'assets/icons/Belleza.svg',
+      color: 'bg-green-100',
+    },
   ];
-
 
   selectCategory(categoria: string): void {
     this.router.navigate(['/catalogo', categoria]);
@@ -34,20 +51,30 @@ export class CategoriasComponent implements AfterViewInit {
 
   ngAfterViewInit() {
     this.updateButtonState();
-    this.carousel.nativeElement.addEventListener('scroll', () => this.updateButtonState());
+    this.carousel.nativeElement.addEventListener('scroll', () =>
+      this.updateButtonState()
+    );
   }
 
   scrollLeft() {
-    this.carousel.nativeElement.scrollBy({ left: -this.carousel.nativeElement.clientWidth, behavior: 'smooth' });
+    this.carousel.nativeElement.scrollBy({
+      left: -this.carousel.nativeElement.clientWidth,
+      behavior: 'smooth',
+    });
   }
 
   scrollRight() {
-    this.carousel.nativeElement.scrollBy({ left: this.carousel.nativeElement.clientWidth, behavior: 'smooth' });
+    this.carousel.nativeElement.scrollBy({
+      left: this.carousel.nativeElement.clientWidth,
+      behavior: 'smooth',
+    });
   }
 
   updateButtonState() {
     const scrollLeft = this.carousel.nativeElement.scrollLeft;
-    const maxScrollLeft = this.carousel.nativeElement.scrollWidth - this.carousel.nativeElement.clientWidth;
+    const maxScrollLeft =
+      this.carousel.nativeElement.scrollWidth -
+      this.carousel.nativeElement.clientWidth;
     this.isScrollLeftDisabled = scrollLeft <= 0;
     this.isScrollRightDisabled = scrollLeft >= maxScrollLeft;
   }
