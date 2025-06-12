5e5ccdef1448fe6bde426a74f8b5185cfb898fd0
mariavaldiviezopingo <luisavaldiviezo24@gmail.com>
2025-06-12 11:13:00 -0500

    actualziacion de rutas en pasarela de pagos

--- src/app/public/form-pago/form-pago.component.ts
+++ src/app/public/form-pago/form-pago.component.ts
@@ -1,13 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-form-pago',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './form-pago.component.html',
   styleUrl: './form-pago.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FormPagoComponent {
+  items = [
+    { nombre: 'Blusa woman', cantidad: 3, precio: 50.00, imagen: 'assets/img/descarga.jpg' },
+    { nombre: 'Pantalón men', cantidad: 1, precio: 300.00, imagen: 'assets/img/descarga.jpg' },
+  ];
+
+  getTotal(): number {
+    return this.items.reduce((total, item) => total + item.precio * item.cantidad, 0);
+  }
 
 }
