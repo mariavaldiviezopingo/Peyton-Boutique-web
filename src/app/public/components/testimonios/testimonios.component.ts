c2b136e0262d2e9eea23bc4b950df96f78a9c837
sandovaldavid <sandovaldavid2201@gmail.com>
2025-06-13 11:16:10 -0500

    mejorar componente de testimonios: agregar animaciones, mejorar diseño y contenido

--- src/app/public/components/testimonios/testimonios.component.ts
+++ src/app/public/components/testimonios/testimonios.component.ts
@@ -1,21 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-    selector: 'app-testimonios',
-    imports: [],
-    templateUrl: './testimonios.component.html',
-    styleUrl: './testimonios.component.css',
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-testimonios',
+  imports: [],
+  templateUrl: './testimonios.component.html',
+  styleUrl: './testimonios.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TestimoniosComponent {
-  // Índice actual del carrusel
-  currentSlide = 0;
-
-  // Array de puntos para los controles de navegación
-  dots = [0, 1, 2];
-
-  // Mover al testimonio correspondiente
-  moveToSlide(index: number) {
-    this.currentSlide = index;
-  }
-}
+export class TestimoniosComponent {}
