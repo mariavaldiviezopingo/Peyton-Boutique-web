2f1a7a618c12bae99c2dce2e31f892a15e87bec9
sandovaldavid <sandovaldavid2201@gmail.com>
2025-06-16 16:26:13 -0500

    Agregar CommonModule a las importaciones del componente AdminDashboard

--- src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-dashboard',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './admin-dashboard.component.html',
   styleUrls: ['./admin-dashboard.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
