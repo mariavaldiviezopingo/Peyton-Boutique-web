ea6d0a8dfc3bfef6ff98fa43a9e3aa0bd3b712ac
sandovaldavid <sandovaldavid2201@gmail.com>
2025-06-19 09:52:27 -0500

    Agregar exportaciones para BreadcrumbComponent y ModalConfirmacionComponent, incluyendo tipos asociados.

--- src/app/admin/components/index.ts
+++ src/app/admin/components/index.ts
@@ -1,2 +1,8 @@
 export { AdminSidebarComponent } from './sidebar/sidebar.component';
 export { AdminHeaderComponent } from './header/header.component';
+export { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+export { ModalConfirmacionComponent } from './modal-confirmacion/modal-confirmacion.component';
+
+//Exportacion de tipos de los componentes
+export type { BreadcrumbItem } from './breadcrumb/breadcrumb.component';
+export type { ModalConfig } from './modal-confirmacion/modal-confirmacion.component';
